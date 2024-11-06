98c5a3620f102f14c1810426a8e769ba25469e82
Priyanshu Rai <150841036+Priyanshu-6@users.noreply.github.com>
2024-11-06 14:06:07 +0000

    updated demo file

--- starting/demo.js
+++ starting/demo.js
@@ -1,1 +1,8 @@
-console.log("wassup!")
+const a = 95
+// a=1223  reassignment not possible of const  
+
+let name="john"
+var username="john_axis"
+acc_pass = "2145654"
+
+console.table([a, name , username, acc_pass])
