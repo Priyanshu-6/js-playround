63995c014299eafbec69f5a316af55ca398f6508
Priyanshu Rai <150841036+Priyanshu-6@users.noreply.github.com>
2024-11-08 15:58:47 +0000

    data conversion - demo1

---
+++ starting/demo1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let loggedin = "s"; 
+// console.log(loggedin) 
+
+let bool_log = Boolean(loggedin)
+// console.log(bool);
+/* 
+ 1 => true , 0 => false , any other number than zero is treatedd as a true in conversion to bool . 
+ 
+  "" => false , "string" => true , anything written as string is treated as a true in conversion to bool, even if it is false written itself in the string we are converting.  "false" => true.
+
+*/
+
+let num = 456;
+
+let s_num = String(num);
+
+console.log(num);
+console.log(s_num);
+console.log(typeof num);
+console.log(typeof s_num);
+
